d0a88dc27362d5a27dd0eac1eb7e64dc5b92db70
BigMacProgrammer <programmer2012@me.com>
2012-08-02 19:21:38 -0700

    Initial Commit

---
+++ Hello World/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Hello World
+//
+//  Created by Darien So on 8/2/12.
+//  Copyright (c) 2012 Nobody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
